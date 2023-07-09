c41512eee01def6eb26045a2f4881cdff1ad71b4
alwin antony <alwinanto5885@gmail.com>
2023-07-09 12:22:19 +0530

    removed unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
 import Excel from './components/excel'
 import Navbar from './components/navbar/Navbar'
-import Login from './components/login/login'
-import Signup from './components/signup/Signup'
 import Footer from './components/footer'
 
 function App() {
